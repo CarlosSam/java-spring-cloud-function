6ca8f4be0468bc054a8f2fc48fb64811230f9d8f
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2019-11-17 20:31:34 -0300

    input output as custom types

--- src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @SpringBootApplication
@@ -17,11 +20,37 @@ public class JavaSpringCloudFunctionApplication {
 	}
 
 	@Bean
-	public Function<String, String> hello(){
-		return name -> {
-			log.info("calling hello with input {}", name);
-			return "Hello " + name;
+	public Function<Input, Output> hello(){
+		return input -> {
+			log.info("calling hello with input {}", input);
+			Output output = new Output();
+			output.setGreeting("Hello " + input.getName() + ", from custom type");
+			return output;
 		};
 	}
 
+}
+
+class Input {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+class Output {
+	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
 }
