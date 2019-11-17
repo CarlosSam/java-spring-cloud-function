d8e56a82d6ca129530ca7adee01e42a0ac56c0b2
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2019-11-17 18:32:45 -0300

    spring initializr

---
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
@@ -0,0 +1,13 @@
+package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCloudFunctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringCloudFunctionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCloudFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
