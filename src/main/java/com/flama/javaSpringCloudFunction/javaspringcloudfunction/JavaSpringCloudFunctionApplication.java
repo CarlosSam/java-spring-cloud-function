2b812fd26a639409a4a47a1fa9d85839a5980929
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2019-11-17 19:43:23 -0300

    run locally

--- src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
@@ -2,12 +2,21 @@ package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class JavaSpringCloudFunctionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringCloudFunctionApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> hello(){
+		return name -> {
+			return "Hello " + name;
+		};
 	}
 
 }
