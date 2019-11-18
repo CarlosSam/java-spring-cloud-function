f9e5d7b1a038a3ef27a08a26dbd4dc272d32f307
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2019-11-18 12:28:04 -0300

    deploy at aws

---
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/Handler.java
@@ -0,0 +1,6 @@
+package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class Handler extends SpringBootRequestHandler<String, String> {
+}
