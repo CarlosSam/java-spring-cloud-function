c1f452bca9013bd74b62c355828482d60000f630
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2019-11-18 13:34:50 -0300

    input and output as maps at aws

--- src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/Handler.java
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/Handler.java
@@ -2,5 +2,7 @@ package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-public class Handler extends SpringBootRequestHandler<String, String> {
+import java.util.Map;
+
+public class Handler extends SpringBootRequestHandler<Map, Map> {
 }
--- src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
+++ src/main/java/com/flama/javaSpringCloudFunction/javaspringcloudfunction/JavaSpringCloudFunctionApplication.java
@@ -1,21 +1,31 @@
 package com.flama.javaSpringCloudFunction.javaspringcloudfunction;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @SpringBootApplication
 public class JavaSpringCloudFunctionApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(JavaSpringCloudFunctionApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringCloudFunctionApplication.class, args);
 	}
 
 	@Bean
-	public Function<String, String> hello(){
-		return name -> {
-			return "Hello " + name;
+	public Function<Map, Map> hello(){
+		return input -> {
+			log.info("hello with input: {}", input);
+			Map output = new HashMap();
+			output.put("greeting", "Hello " + input.get("name") + " from a map");
+			return output;
 		};
 	}
 
